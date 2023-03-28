e3c7d4a40cece4742020ed98c75105775c0744df
Yamcha7 <56405714+Yamcha7@users.noreply.github.com>
2023-03-28 11:59:48 +0530

    Add files via upload

--- HackTheBoxCTF_Apocalypse/crypto/Multipage_recycling/source.py
+++ HackTheBoxCTF_Apocalypse/crypto/Multipage_recycling/source.py
@@ -1,17 +1,54 @@
-from Crypto.Util.number import bytes_to_long
-from base64 import b64encode
+from Crypto.Cipher import AES
+from Crypto.Util.Padding import pad
+import random, os
 
-FLAG = b"HTB{??????????}"
+FLAG = b'HTB{??????????????????????}'
 
 
-def encode(message):
-    return hex(bytes_to_long(b64encode(message)))
+class CAES:
+
+    def __init__(self):
+        self.key = os.urandom(16)
+        self.cipher = AES.new(self.key, AES.MODE_ECB)
+
+    def blockify(self, message, size):
+        return [message[i:i + size] for i in range(0, len(message), size)]
+
+    def xor(self, a, b):
+        return b''.join([bytes([_a ^ _b]) for _a, _b in zip(a, b)])
+
+    def encrypt(self, message):
+        iv = os.urandom(16)
+
+        ciphertext = b''
+        plaintext = iv
+
+        blocks = self.blockify(message, 16)
+        for block in blocks:
+            ct = self.cipher.encrypt(plaintext)
+            encrypted_block = self.xor(block, ct)
+            ciphertext += encrypted_block
+            plaintext = encrypted_block
+
+        return ciphertext
+
+    def leak(self, blocks):
+        r = random.randint(0, len(blocks) - 2)
+        leak = [self.cipher.encrypt(blocks[i]).hex() for i in [r, r + 1]]
+        return r, leak
 
 
 def main():
-    encoded_flag = encode(FLAG)
-    with open("output.txt", "w") as f:
-        f.write(encoded_flag)
+    aes = CAES()
+    message = pad(FLAG * 4, 16)
+
+    ciphertext = aes.encrypt(message)
+    ciphertext_blocks = aes.blockify(ciphertext, 16)
+
+    r, leak = aes.leak(ciphertext_blocks)
+
+    with open('output.txt', 'w') as f:
+        f.write(f'ct = {ciphertext.hex()}\nr = {r}\nphrases = {leak}\n')
 
 
 if __name__ == "__main__":
